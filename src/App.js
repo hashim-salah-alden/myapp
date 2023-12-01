d24317009201791f10cb4fe44791b62aeee5fc79
hashim-salah-alden <mrinvincible14@yahoo.com>
2023-12-01 14:13:11 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>My First React App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
